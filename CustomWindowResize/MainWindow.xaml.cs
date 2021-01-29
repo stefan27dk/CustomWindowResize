180817cc054ec7ac0750625c897e0c3c47b8e16f
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-01-29 13:07:25 +0100

    Add project files.

---
+++ CustomWindowResize/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace CustomWindowResize
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var str = (string)((Thumb)sender).Tag;
+
+            if (str.Contains("T"))
+            {
+                Content.Height = Math.Min(Math.Max(Content.MinHeight, Content.ActualHeight - e.VerticalChange), Content.MaxHeight);
+                Canvas.SetTop(Content, Canvas.GetTop(Content) - Content.Height + Content.ActualHeight);
+            }
+            if (str.Contains("L"))
+            {
+                Content.Width = Math.Min(Math.Max(Content.MinWidth, Content.ActualWidth - e.HorizontalChange), Content.MaxWidth);
+                Canvas.SetLeft(Content, Canvas.GetLeft(Content) - Content.Width + Content.ActualWidth);
+            }
+            if (str.Contains("B"))
+            {
+                Content.Height = Math.Min(Math.Max(Content.MinHeight, Content.ActualHeight + e.VerticalChange), Content.MaxHeight);
+            }
+            if (str.Contains("R"))
+            {
+                Content.Width = Math.Min(Math.Max(Content.MinWidth, Content.ActualWidth + e.HorizontalChange), Content.MaxWidth);
+            }
+            if (str.Contains("M"))
+            {
+                Canvas.SetTop(Content, Canvas.GetTop(Content) + e.VerticalChange);
+                Canvas.SetLeft(Content, Canvas.GetLeft(Content) + e.HorizontalChange);
+            }
+
+            e.Handled = true;
+        }
+    }
+}
